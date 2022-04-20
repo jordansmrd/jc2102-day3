8d71a61cefce6fe9a07584acad93011c1718fb8e
jordansmrd <jordansumardi@hotmail.com>
2022-04-20 18:49:32 +0700

    Initial commit

---
+++ array.js
@@ -0,0 +1,14 @@
+let arr = ["jordan", "udin", "kipak"]
+arr.push("ujang") //add array
+arr.push(19) //add array
+arr.pop() //delete last array
+
+for(let i = 0;i< arr.length;i++)
+{
+    //delete last array
+    console.log
+  console.log(arr[i])  
+}
+
+
+console.log(arr)
